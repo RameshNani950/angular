1d98a51af8aba03d6e9956c54be0dbd0d6f5df33
RameshNani9502 <rameshyadavkattebona@gmail.com>
2023-10-14 18:12:00 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+var a = 10;
+console.log(a);
+console.log(Math.floor(a));
+if (a / 2) {
+    console.log(a, "is a even number");
+}
+else {
+    console.log(a, "is a odd number");
+}
---
+++ index.ts
@@ -0,0 +1,13 @@
+let a:number =10;
+console.log(a);
+console.log(Math.floor(a));
+if(a/2)
+{
+    console.log(a,"is a even number");
+}
+else
+{
+    console.log(a,"is a odd number");
+}
+
+
